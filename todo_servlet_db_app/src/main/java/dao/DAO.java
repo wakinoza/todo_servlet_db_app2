18b8731cb97965850d1cfac88c6523a5fc7dd0fe
wakinoza <wakinoza@gmail.com>
2025-12-08 08:46:53 +0900

    CheckStyleの指摘を修正

--- todo_servlet_db_app/src/main/java/dao/DAO.java
+++ todo_servlet_db_app/src/main/java/dao/DAO.java
@@ -4,9 +4,16 @@ import java.sql.Connection;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+/** . DB接続をつかさどるクラス */
 public class DAO {
   static DataSource ds;
 
+  /**
+   * . DBと接続するメソッド
+   *
+   * @return Connectionインスタンス
+   * @throws Exception DBと接続できなかった場合
+   */
   public Connection getConnection() throws Exception {
     if (ds == null) {
       InitialContext ic = new InitialContext();
