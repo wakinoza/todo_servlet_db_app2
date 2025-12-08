021de3dd8edd4adcf912dc1fc36eeca77c2ca983
wakinoza <wakinoza@gmail.com>
2025-12-08 10:02:18 +0900

    checkstyleの警告を修正

--- todo_servlet_db_app/src/main/java/model/LoginLogic.java
+++ todo_servlet_db_app/src/main/java/model/LoginLogic.java
@@ -3,9 +3,19 @@ package model;
 import bean.User;
 import dao.UserDAO;
 
+/**
+ * . ログイン処理を行うビジネスクラス
+ */
 public class LoginLogic {
+  /**
+   * . 指定されたUserがDBテーブルにあるかを確認するメソッド
+   *
+   * @param name ユーザー名
+   * @param pass パスワード
+   * @return 指定されたUserがDBテーブルにあればUseインスタンスを、なければnullを返す
+   */
   public User serch(String name, String pass) {
-    UserDAO userDAO = new UserDAO();
-    return userDAO.select(name, pass);
+    UserDAO userDao = new UserDAO();
+    return userDao.select(name, pass);
   }
 }
