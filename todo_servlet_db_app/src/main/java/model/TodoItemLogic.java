02bb2ae34219b6db8cbd3b0b2d5433cff4103ecf
wakinoza <wakinoza@gmail.com>
2025-12-08 10:12:07 +0900

    checkstyleの警告を修正

--- todo_servlet_db_app/src/main/java/model/TodoItemLogic.java
+++ todo_servlet_db_app/src/main/java/model/TodoItemLogic.java
@@ -4,8 +4,17 @@ import java.util.List;
 import bean.TodoItem;
 import dao.TodoItemDAO;
 
+/**
+ * . toditemインスタンスの処理を行うビジネスクラス
+ */
 public class TodoItemLogic {
 
+  /**
+   * . todoItemインスタンスを作成するめそっど
+   *
+   * @param text テキスト入力欄の文字列情報
+   * @return 作成に成功すればTodoItemインスタンスを、失敗すればnullを返す
+   */
   public TodoItem create(String text) {
     if (text != null && !text.isEmpty()) {
       TodoItem todoItem = new TodoItem(text);
@@ -16,18 +25,35 @@ public class TodoItemLogic {
     }
   }
 
+  /**
+   * . 指定されたTodoItemインスタンスをDBに挿入するメソッド
+   *
+   * @param todoItem 追加するTodoItemインスタンス
+   * @return 挿入が成功したかを示す真偽値
+   */
   public boolean add(TodoItem todoItem) {
-    TodoItemDAO todoItemDAO = new TodoItemDAO();
-    return todoItemDAO.insert(todoItem);
+    TodoItemDAO todoItemDao = new TodoItemDAO();
+    return todoItemDao.insert(todoItem);
   }
 
+  /**
+   * . 指定されたTodoItemインスタンスの進捗情報を更新するメソッド
+   *
+   * @param id TodoItemインスタンスのID
+   * @return 更新が成功したかを示す真偽値
+   */
   public boolean updateProgress(String id) {
-    TodoItemDAO todoItemDAO = new TodoItemDAO();
-    return todoItemDAO.updateProgress(id);
+    TodoItemDAO todoItemDao = new TodoItemDAO();
+    return todoItemDao.updateProgress(id);
   }
 
+  /**
+   * . すべてのTodoItemインスタンスを取得するメソッド
+   *
+   * @return TodoItemインスタンスを格納したList
+   */
   public List<TodoItem> getAllTodoItem() {
-    TodoItemDAO todoItemDAO = new TodoItemDAO();
-    return todoItemDAO.selectAll();
+    TodoItemDAO todoItemDao = new TodoItemDAO();
+    return todoItemDao.selectAll();
   }
 }
