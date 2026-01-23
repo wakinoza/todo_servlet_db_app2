773721f0183b2dbfbd59bb5ad800a3308df3b577
wakinoza <wakinoza@gmail.com>
2026-01-23 15:47:54 +0900

    DAOクラスのＤataSoueceを検索専用と更新専用に分ける

--- todo_servlet_db_app/src/main/java/dao/DAO.java
+++ todo_servlet_db_app/src/main/java/dao/DAO.java
@@ -4,23 +4,32 @@ import java.sql.Connection;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
-/** . DB接続をつかさどるクラス */
+/** . DB接続をつかさどる共通クラス. */
 public class DAO {
-  static DataSource ds;
+  static DataSource readDs; // 検索専用
+  static DataSource writeDs; // 更新用
 
   /**
-   * . DBと接続するメソッド
-   *
-   * @return Connectionインスタンス
-   * @throws Exception DBと接続できなかった場合
+   * 検索専用（SELECTのみ）のConnectionを取得.
    */
-  public Connection getConnection() throws Exception {
-    if (ds == null) {
+  public Connection getReadConnection() throws Exception {
+    if (readDs == null) {
       InitialContext ic = new InitialContext();
-      ds = (DataSource) ic.lookup("java:comp/env/jdbc/test_db");
+      // context.xmlで定義した検索用リソース名を指定
+      readDs = (DataSource) ic.lookup("java:comp/env/jdbc/test_db_read");
     }
-
-    return ds.getConnection();
+    return readDs.getConnection();
   }
 
+  /**
+   * 更新用（INSERT/UPDATE/DELETE）のConnectionを取得.
+   */
+  public Connection getWriteConnection() throws Exception {
+    if (writeDs == null) {
+      InitialContext ic = new InitialContext();
+      // context.xmlで定義した更新用リソース名を指定
+      writeDs = (DataSource) ic.lookup("java:comp/env/jdbc/test_db_write");
+    }
+    return writeDs.getConnection();
+  }
 }
