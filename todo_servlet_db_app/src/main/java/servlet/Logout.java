cf0e6b9b656e7c276a8e021acb2abc68c7141d74
wakinoza <wakinoza@gmail.com>
2026-01-30 10:47:00 +0900

    ContentType設定の追加

--- todo_servlet_db_app/src/main/java/servlet/Logout.java
+++ todo_servlet_db_app/src/main/java/servlet/Logout.java
@@ -1,6 +1,5 @@
 package servlet;
 
-import java.io.IOException;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,6 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * . ログアウト処理を司るサーブレットクラス
@@ -22,6 +22,8 @@ public class Logout extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
+    request.setCharacterEncoding("UTF-8");
+    response.setContentType("text/html; charset=UTF-8");
     HttpSession session = request.getSession();
     session.invalidate();
 
