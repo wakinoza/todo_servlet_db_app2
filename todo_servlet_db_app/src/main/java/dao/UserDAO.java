880c86a98c90caf1282b284b3eb7421e7d774ae7
wakinoza <wakinoza@gmail.com>
2026-02-02 17:18:47 +0900

    UserDAOクラスにハッシュ化を実装

--- todo_servlet_db_app/src/main/java/dao/UserDAO.java
+++ todo_servlet_db_app/src/main/java/dao/UserDAO.java
@@ -4,41 +4,44 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import bean.User;
+import util.PasswordUtil;
 
-/** . todo_usersテーブルの操作を行うDAOクラス */
+/** todo_usersテーブルの操作を行うDAOクラス */
 public class UserDAO extends DAO {
+
   /**
-   * . テーブルに指定されたUserが存在するかを確認するメソッド
+   * テーブルに指定されたUserが存在するかを確認するメソッド
    *
    * @param name ユーザー名
-   * @param pass パスワード
-   * @return 指定されたユーザーがいればそのUserインスタンスを、いなければnullを返す
+   * @param pass 入力された平文パスワード
+   * @return 認証成功ならUserインスタンスを、失敗ならnullを返す
    */
   public User select(String name, String pass) {
     User user = null;
 
+    String sql = "SELECT id, name, password_hash FROM todo_users WHERE name = ?";
+
     try (Connection con = getReadConnection()) {
-      String sql = "SELECT id, name FROM todo_users WHERE name=? AND password=?";
-      try (PreparedStatement st = con.prepareStatement(sql);) {
+      try (PreparedStatement st = con.prepareStatement(sql)) {
         st.setString(1, name);
-        st.setString(2, pass);
 
         try (ResultSet rs = st.executeQuery()) {
           if (rs.next()) {
-            user = new User();
-            user.setId(rs.getInt("id"));
-            user.setName(rs.getString("name"));
+
+            String dbHash = rs.getString("password_hash");
+
+            if (PasswordUtil.check(pass, dbHash)) {
+              user = new User();
+              user.setId(rs.getInt("id"));
+              user.setName(rs.getString("name"));
+            }
           }
         }
       }
-
-
     } catch (Exception e) {
       e.printStackTrace();
     }
 
     return user;
-
   }
-
 }
