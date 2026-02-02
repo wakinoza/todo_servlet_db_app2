2c3feb447f5588cc6816bdc6d99604f17fdba321
wakinoza <wakinoza@gmail.com>
2026-02-02 17:25:28 +0900

    javadocを記入した

--- todo_servlet_db_app/src/main/java/util/PasswordUtil.java
+++ todo_servlet_db_app/src/main/java/util/PasswordUtil.java
@@ -2,22 +2,40 @@ package util;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/**
+ * パスワードをハッシュ化するユーティリティクラス.
+ */
 public class PasswordUtil {
   private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
-  // ハッシュ化用
+  /**
+   * パスワードをハッシュ化するメソッド.
+   * 
+   * @param rawPassword ユーザー入力のパスワード
+   * @return ハッシュ値
+   */
   public static String hash(String rawPassword) {
     return encoder.encode(rawPassword);
   }
 
-  // 照合用
+  /**
+   * 入力したパスワードと、DBのハッシュ値とを比較するメソッド.
+   * 
+   * @param rawPassword ユーザー入力のパスワード
+   * @param hashed DBに格納されたハッシュ値
+   * @return 照合結果
+   */
   public static boolean check(String rawPassword, String hashed) {
     return encoder.matches(rawPassword, hashed);
   }
 
-  public static void main(String[] args) {
-    String myPass = "yamada_password";
-    System.out.println("生成されたハッシュ値: " + hash(myPass));
-  }
+  // /**
+  // * ハッシュ値を出力するメソッド.
+  // * @param args
+  // */
+  // public static void main(String[] args) {
+  // String myPass = "yamada_password";
+  // System.out.println("生成されたハッシュ値: " + hash(myPass));
+  // }
 
 }
